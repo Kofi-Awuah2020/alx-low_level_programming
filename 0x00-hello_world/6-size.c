a7a96a328f66cad759df9a16c986e88ae84eeb95
Kofi <kofiawuah.a@gmail.com>
2023-10-06 04:32:45 +0000

    Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the size of various data types
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("Size of a char: 1 byte(s)");
+	puts("Size of an int: 4 byte(s)");
+	puts("Size of a long int: 4 byte(s)");
+	puts("Size of a long long: 8 byte(s)");
+	puts("Size of a float: 4 byte(s)");
+	return (0);
+}
