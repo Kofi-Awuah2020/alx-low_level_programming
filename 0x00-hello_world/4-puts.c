fbcfba5f1601c62be30adc91259b9f0b1f204699
Kofi <kofiawuah.a@gmail.com>
2023-10-06 03:02:28 +0000

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+/**
+ * 4-puts.c
+ *
+ * Description: prints a string of text followed by a new line.
+ *
+ * Author: Kofi Amankwah Awuah
+ *
+ * Date: 06/10/2023
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 for success and non-zero for failure
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	
+	return (0);
+}
+
+
