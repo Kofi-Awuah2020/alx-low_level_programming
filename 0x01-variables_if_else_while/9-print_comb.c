11907e178c45c1297a2e6c42fd867c280b919bad
Kofi <kofiawuah.a@gmail.com>
2023-10-07 02:45:14 +0000

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints 0 - 9 separated by ',' and ' '
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int digit;
+	int comma;
+	int space;
+
+	digit = 48;
+	comma = 44;
+	space = 32;
+	while (digit < 58)
+	{
+		putchar(digit);
+		putchar(comma);
+		putchar(space);
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
