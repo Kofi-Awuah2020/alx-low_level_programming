01159c5e1ee4a359abf07ae6e845cca763991423
Kofi <kofiawuah.a@gmail.com>
2023-10-07 02:14:02 +0000

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-    int x;
-    
-    x = 48;
-    while (x < 58)
-    {
-	putchar(x);
-	x++;
-    }
-    putchar('\n');
-    return (0);
+	int x;
+
+	x = 48;
+	while (x < 58)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
 }
 
