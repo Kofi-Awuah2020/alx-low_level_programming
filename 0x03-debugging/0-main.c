71259d4991eaf7283e5da0ab92afedca31575295
Kofi <kofiawuah.a@gmail.com>
2023-10-13 12:31:42 +0000

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main -tests function that prints if integer is +ve or -ve
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative();
+	return (0);
+}
