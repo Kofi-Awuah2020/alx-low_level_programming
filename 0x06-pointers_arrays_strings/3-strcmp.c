d8e4e077675762a30921ce7c1e84d2190d1570bd
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2023-10-19 13:18:05 +0000

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,18 +11,20 @@
 int _strcmp(char *s1, char *s2)
 {
 	int x;
+	int calc;
 
 	for (x = 0; s1[x] != '\0' || s2[x] != '\0'; x++)
 	{
 		if (s1[x] != s2[x])
 		{
-			int calc = s1[x] - s2[x];
-
-			return (calc);
+			calc = s1[x] - s2[x];
+			break;
 		}
 		else
 		{
-			return (0);
+			calc = 0;
+			break;
 		}
 	}
+	return (calc);
 }
