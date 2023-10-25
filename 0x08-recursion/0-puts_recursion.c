12ef1d34399ca48293cdfbc9e720c076ad8a0966
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2023-10-25 18:00:47 +0000

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - function prints a string followed by '\n'
+ * @s: pointer to string to be printed
+ *
+ * Return: Void
+ */
+void _puts_recursion(char *s)
+{
+	if (s == '\0')
+	{
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(++s);
+}
