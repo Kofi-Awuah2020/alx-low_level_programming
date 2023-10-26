6604d61f2165395dc3c22254042151c75fbd0423
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2023-10-26 03:30:19 +0000

    Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * appx_root - checks root starting from 1 till root * root = n.
+ * @root: variable for caluclating square root
+ * @n: number that sqaure root most be calculated from
+ *
+ * Return: result of calculation
+ */
+int appx_root(int root, int n)
+{
+	if (root * root > n)
+	{
+		return (-1);
+	}
+	if (root * root == n)
+	{
+		return (n);
+	}
+	return (appx_root(root + 1, n));
+
+/**
+ * _sqrt_recursion - returns natural square root of a number
+ * @n: input number
+ *
+ * Return: result
+ */
+int _sqrt_recursion(int n)
+{
+	return (appx root(1, n));
+}
