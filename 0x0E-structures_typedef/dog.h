98192882e3f9cd73f28abc451377060353ac1284
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2023-11-07 05:31:28 +0000

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,22 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog {
+
+/**
+ * struct dog - stores a dogs data
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: the name of the dog owner
+ *
+ * Description: This is a data structure that hold a dogs info
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
