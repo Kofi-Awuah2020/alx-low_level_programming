2550010c993fa4d4378a381addddaf9ca9701b7b
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2023-11-09 21:52:18 +0000

    Updated Prototypes

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,10 +2,6 @@
 #define FUNCTION_POINTERS_H
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void(*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-3-op_functions.c;
-3-get_op_func.c;
-100-main_opcodes.c;
 
 #endif
