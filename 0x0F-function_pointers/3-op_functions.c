33a1e990750629197a43c52a81fbd18ebadc6c72
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2023-11-10 22:25:01 +0000

    Changed the edge case

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -47,7 +47,7 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-	if (a == 0 || b == 0)
+	if (b == 0)
 	{
 		puts("Error");
 		exit(100);
@@ -64,7 +64,7 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-	if (a == 0 || b == 0)
+	if (b == 0)
 	{
 		puts("Error");
 		exit(100);
