9f9c123794b4e03b4d370c7b67fd1b365a6d5481
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2023-11-21 09:15:53 +0000

    Print List

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_list - function prints all the elements of a list
+ * @h: structure to be printed
+ *
+ * Return: Number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *ptr = h;
+
+	if (ptr == NULL)
+	{
+		return (0);
+	}
+	while (ptr != NULL)
+	{
+		count++;
+		printf("[%u] ", ptr->len);
+		printf("%s\n", (ptr->str == NULL) ? "null" : ptr->str);
+		ptr = ptr->next;
+	}
+	return (count);
+}
