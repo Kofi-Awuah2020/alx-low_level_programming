9e248afce054d6fd1e31f6d59aa782d49052e43b
Kofi-Awuah2020 <kofiawuah.a@gmail.com>
2023-11-28 14:13:19 +0000

    function reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -38,7 +38,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0); /* Read failed */
 	}
-	bytesWritten = write(STDOUT_FILENO, buffer, bytesWritten);
+	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
 	if (bytesWritten != bytesRead || bytesWritten == -1)
 	{
 		return (0); /* Write failed to print number of bytes */
